41cf256024eac9c80dd88f081e2289832d2ff49c
guilherme eli <guilhermeeli96@gmail.com>
2024-05-27 05:16:55 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program 
+{
+    static void Main(string[] args)
+    {
+        // Criar instâncias de Nokia e Iphone
+        Smartphone meuNokia = new Nokia("123456", "Nokia 3310", "111111111111111", 64);
+        Smartphone meuIphone = new Iphone("654321", "iPhone X", "222222222222222", 256);
+
+        // Usar os métodos das instâncias
+        meuNokia.Ligar();
+        meuNokia.InstalarAplicativo("WhatsApp");
+
+        meuIphone.ReceberLigacao();
+        meuIphone.InstalarAplicativo("Instagram");
+    }
+}
+
+
+
